71a99a22294b04f566be6e6348bce5d7df8486fb
conserpark <120622892+conserpark@users.noreply.github.com>
2023-06-14 21:22:02 +0900

    Database

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,44 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => {
-  console.log(req.params);
-  return res.send("Delete Video");
+let videos = [
+  {
+    title: "Hello",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 1,
+    id: 1,
+  },
+  {
+    title: "Video #2",
+    rating: 3,
+    comments: 6,
+    createdAt: "10 minutes ago",
+    views: 54,
+    id: 2,
+  },
+  {
+    title: "Whatsup",
+    rating: 4,
+    comments: 1,
+    createdAt: "22 minutes ago",
+    views: 154,
+    id: 3,
+  },
+];
+
+export const trending = (req, res) => res.render("home", { pageTitle: "Home", videos });
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
+};
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("edit", { pageTitle: `Editing ${video.title}`, video });
+};
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
 };
